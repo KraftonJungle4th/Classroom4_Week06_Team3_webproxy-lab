70b4484db608423bacbf49ebd30e6e00ac840196
yunsejin <ysj1375@github.com>
2024-03-24 20:23:44 +0900

    update_tiny

--- tiny/tiny.c
+++ tiny/tiny.c
@@ -258,14 +258,3 @@ void clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longms
   Rio_writen(fd, buf, strlen(buf));
 }
 /* $end clienterror */
-void echo(int connfd) {
-  size_t n;
-  char buf[MAXLINE];
-  rio_t rio;
-
-  Rio_readinitb(&rio, connfd);
-  while ((n = Rio_readlineb(&rio, buf, MAXLINE)) != 0) {
-    if (strcmp(buf, "\r\n") == 0) break;
-    Rio_writen(connfd, buf, n);
-  }
-}
